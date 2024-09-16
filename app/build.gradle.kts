9a4c63ab7becbed3216a261ca74b838a97455673
Shobhit Prajapati <devtestidshobhit@gmail.com>
2024-09-16 11:32:40 +0530

    Font Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
 
     implementation("androidx.compose.ui:ui:1.7.1")
     implementation("androidx.compose.material:material:1.7.1")
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
 
 
     implementation(libs.androidx.core.ktx)
